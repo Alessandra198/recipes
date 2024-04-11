9b2bebd47885a89e2109a4f8beefb7e007bb46e2
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-11 18:34:55 +0200

    Imported axios and created functions to handle the event of the form

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
-    return (
-        <div className="Search">
-            <form>
-                <input type="search" placeholder="Ex. chicken, carbonara.." className="input-search"/>
-                <input type="button" value="Search" className="input-button"/>
-            </form>
-        </div>
-    );
+  let [text, setText] = useState(null);
+
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(text);
+  }
+  return (
+    <div className="Search">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Ex. chicken, carbonara.."
+          className="input-search"
+          onChange={handleChange}
+        />
+        <input type="button" value="Search" className="input-button" />
+      </form>
+    </div>
+  );
 }
