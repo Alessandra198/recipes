4d90e25609c22ce950208a1af458b2bf88622516
Alessandra198 <rinaldoalessandra@gmail.com>
2024-04-11 21:36:01 +0200

    Added .map to loop in the object

--- src/Recipes.js
+++ src/Recipes.js
@@ -2,9 +2,17 @@ import React from "react";
 
 export default function Recipes(props) {
   if (props.recipes) {
+    console.log(props.recipes);
     return (
       <div className="Recipes">
         <h2>We've found some results for you😋</h2>
+        {props.recipes.results.map(function (recipe, index) {
+          return (
+            <div key={index}>
+              <img src={recipe.image} alt={recipe.title} />
+            </div>
+          );
+        })}
       </div>
     );
   } else {
